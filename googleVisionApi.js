c27588c5c90a490485041b2202b2e50c946333f8
Smokja <cetkovic@gmx.ch>
2020-05-13 05:37:36 +0200

    Corrected Request body and removed unnecessary await

--- googleVisionApi.js
+++ googleVisionApi.js
@@ -5,17 +5,19 @@ export class GoogleVisionApi {
     static apiKey = "?key=AIzaSyB5WVcfCzsxhCRfh34jTiubDyEOnP5pXYc";
     static requestURL = urljoin(GoogleVisionApi.imageRecognitionURL, GoogleVisionApi.apiKey);
     static async postImage(base64StringOfImage, type = "LABEL_DETECTION", maxResults = 5) {
-        const requestBody = [
-            {
-                image: { content: base64StringOfImage }, // Example of converting a buffer to base64string: Buffer.from(fs.readFileSync(imagePath)).toString("base64")
-                features: [
-                    {
-                        type: type,
-                        maxResults: maxResults
-                    }
-                ]
-            }
-        ];
+        const requestBody = {
+            requests: [
+                {
+                    image: { content: base64StringOfImage }, // Example of converting a buffer to base64string: Buffer.from(fs.readFileSync(imagePath)).toString("base64")
+                    features: [
+                        {
+                            type: type,
+                            maxResults: maxResults
+                        }
+                    ]
+                }
+            ]
+        };
 
         const response = await fetch(GoogleVisionApi.requestURL, {
             method: 'POST',
@@ -25,6 +27,6 @@ export class GoogleVisionApi {
             body: JSON.stringify(requestBody)
         });
 
-        return await response.json();
+        return response.json();
     }
 }
