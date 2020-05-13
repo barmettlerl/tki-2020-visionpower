3b146d7b239740c3ea0b4508678a94c12c336e0f
Smokja <cetkovic@gmx.ch>
2020-05-13 05:06:15 +0200

    Added googleVisionApi.js
    Can be used in the backend to make a request to the google vision api

---
+++ googleVisionApi.js
@@ -0,0 +1,30 @@
+import urljoin from 'url-join';
+
+export class GoogleVisionApi {
+    static imageRecognitionURL = "https://vision.googleapis.com/v1/images:annotate";
+    static apiKey = "?key=AIzaSyB5WVcfCzsxhCRfh34jTiubDyEOnP5pXYc";
+    static requestURL = urljoin(ApiHelper.imageRecognitionURL, ApiHelper.apiKey);
+    static async postImage(base64StringOfImage, type = "LABEL_DETECTION", maxResults = 5) {
+        const requestBody = [
+            {
+                image: { content: base64StringOfImage }, // Example of converting a buffer to base64string: Buffer.from(fs.readFileSync(imagePath)).toString("base64")
+                features: [
+                    {
+                        type: type,
+                        maxResults: maxResults
+                    }
+                ]
+            }
+        ];
+
+        const response = await fetch(ApiHelper.requestURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
+        });
+
+        return await response.json();
+    }
+}
